80fdd9bf6edca147b036a5a77a2459f25ffda94a
Aniruddh Dadhich <aniruddhddh17@gmail.com>
2023-08-14 03:42:16 +0530

    Binary Search easy problems done

--- Binary Search/src/BinarySearch.java
+++ Binary Search/src/BinarySearch.java
@@ -1,9 +1,8 @@
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] arr = {3,1};  // 0
-
-        int id = searchInRotatedUnique(arr, 1);
+        int[] arr = {1,5,4,3,2,6,8,4,3,1};  // ans = 8, index = 6
+        int id = peakElement(arr);
         System.out.println(id);
     }
 
@@ -141,7 +140,7 @@ public class BinarySearch {
                 ans = i;
                 break;
             }
-            if(arr[i]>=arr[start]){                   //{3,1};  1
+            if(arr[i]>=arr[start]){
                 //left half is sorted
                 if(value>=arr[start] && value<=arr[i]){
                     end = i-1;
@@ -165,5 +164,190 @@ public class BinarySearch {
     }
 
     // 9. Find the value in rotated Sorted Array(may contain duplicate value)
+    static boolean searchInRotatedDuplicate(int[] arr, int value){
+        int start = 0;
+        int end = arr.length - 1;
 
+        while(start<=end){
+            int i = (start+end)/2;
+            if(arr[i]==value){
+                return true;
+            }
+
+            // edge case which didn't exist in unique element array
+            if(arr[i]==arr[start] && arr[i] == arr[end]){
+                start++;
+                end--;
+                continue;
+            }
+
+
+            if(arr[i]>=arr[start]){
+                //left half is sorted
+                if(value>=arr[start] && value<=arr[i]){
+                    end = i-1;
+                }
+                else{
+                    start = i+1;
+                }
+            }
+            else{
+                //right half is sorted
+                if(value<=arr[end] && value>=arr[i]){
+                    start = i+1;
+                }
+                else{
+                    end = i-1;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    //10. Find the minimum in rotated sorted array(unique values)
+    static int searchMinimumInRotated(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+        while(start<=end){
+            int i = (start+end)/2;
+
+            if(arr[i]>=arr[start]){
+                // left half is sorted
+                min = Math.min(min,arr[start]);
+                // now discard the left half
+                start = i+1;
+            }
+            else{
+                // right half is sorted
+                min = Math.min(min,arr[i]);
+                // now discard the right half
+                end = i-1;
+            }
+        }
+        return min;
+    }
+
+    // 11. Find the minimum no. of times array is rotated.
+    // approach - find the index of minimum number in array that's the answer.
+    static int numberOfRotations(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+        int mini = -1;
+        while(start<=end){            //{1,2,3}
+            int i = (start+end)/2;
+            if(arr[i]>=arr[start]){
+                // left half is sorted
+                if(arr[start]<min) {
+                    min = arr[start];
+                    mini = start;
+                }
+                // now discard the left half
+                start = i+1;
+            }
+            else{
+                // right half is sorted
+                if(arr[i]<min) {
+                    min = arr[i];
+                    mini = i;
+                }
+                // now discard the right half
+                end = i-1;
+            }
+        }
+        return mini;
+    }
+
+    // 12. all elements are occurring twice except one, find that single element. in a sorted array
+    // {1,1,2,2,3,4,4,5,5,6,6}
+    // approach => all pair elements should have even index then odd index, but when the single one comes
+    // it changes to their indices as odd-even. so if you hit odd-even then go left otherwise go right.
+    static int findSingleElement(int[] arr){
+        int n = arr.length;
+
+        //edge cases
+        //1. if only one element
+        if(n == 1){
+            return arr[0];
+        }
+        // 2. if ans at boundaries
+        if(arr[0]!=arr[1]) return arr[0];
+        if(arr[n-1]!= arr[n-2]) return arr[n-1];
+
+        //
+        int start = 1;
+        int end = n-2;
+        int ans = -1; // no single element then -1
+
+        while(start<=end){
+            int i = (start+end)/2;
+
+            // check for ans
+            if(arr[i]!=arr[i+1] && arr[i]!=arr[i-1]){
+                return arr[i];
+            }
+            else{
+                // you have hit a pair
+                // is this the first one && it's index is even => this is left, go to the right
+                if(arr[i]==arr[i+1] && i%2==0){
+                    start = i+2;
+                }
+                // is this the second one && it's index is odd => this is left, go to the right
+                else if(arr[i]==arr[i-1] && i%2!=0){
+                    start = i+1;
+                }
+                // is this the first one and it's index is odd => this is right, to the left;
+                else if(arr[i]==arr[i+1] && i%2!=0){
+                    end = i-1;
+                }
+                // is this the second one and it's index is even = any way last case
+                else{
+                    end = i-2;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //13. Find the peak element index, if multiple then return any one of them
+    static int peakElement(int[] arr){
+        int n = arr.length;
+        //edge case
+        // only one element
+        if(n==1){
+            return 0;
+        }
+        // boundary elements ==> easy cases to return and get answer
+        if(arr[0]>arr[1]){
+            return 0;
+        }
+        if(arr[n-1]>arr[n-2]){
+            return n-1;
+        }
+
+        //main implementation
+        int start = 1;
+        int end = n-2;
+
+        while(start<=end){
+            int i = (start+end)/2;
+
+            // check ans
+            if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+                return i;
+            }
+            // if not the ans
+            if(arr[i]>arr[i-1]){
+                // we are on increment curve, peak can't be on left, go right
+                start = i+1;
+            }
+            else{
+                // we are on decrement curve, peak can't be on right, go left
+                end = i-1;
+            }
+        }
+        return -1;
+    }
 }
