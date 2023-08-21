2b6e5b6d7799bf69e4e3a273bc74828c0bb3175a
Aniruddh Dadhich <aniruddhddh17@gmail.com>
2023-08-21 09:57:36 +0530

    Linked List 3rd day

--- Linked List/src/Problems.java
+++ Linked List/src/Problems.java
@@ -1,6 +1,6 @@
 public class Problems {
 
-
+    // compiling here is little complicated but understand the logic here.
     public static void main(String[] args) {
 
         // Non-runnable solutions, solve in leetcode
@@ -67,11 +67,11 @@ public class Problems {
 //        ListNode slow = head;
 //        ListNode fast = head;
 //
-//        // //only one element
-//        // if(head.next != null){
+//        // // if only one element exists in the linked list => two cases either it points to null or itself.
+//        // if(head.next != null){   // single
 //        //     return false;
 //        // }
-//        // if(head.next == head){
+//        // if(head.next == head){    // circular
 //        //     return true;
 //        // }
 //
@@ -94,7 +94,8 @@ public class Problems {
 //                slow = slow.next;
 //                fast = fast.next.next;
 //                if(slow==fast){
-//                    //keep the fast at this position and move slow
+//                    //The moment you find the cycle then keep the fast at this position and move slow
+                      // Slow will cover all of the points of circle.
 //                    int count = 0;
 //                    do{
 //                        slow = slow.next;
@@ -106,6 +107,140 @@ public class Problems {
 //            return 0;
 //        }
 
+
+//        Q.5. Finding the starting of the cycle in linked list
+        // approach
+// 1. Find if cycle exists
+// 2. if yes then find the length,
+//. 3. then start a pointer at head another one at cycle length ahead of the head.
+// keep moving them one place at a time to meet at head.
+//        public ListNode detectCycle(ListNode head) {
+//            ListNode slow = head;
+//            ListNode fast = head;
+//            while(fast != null && fast.next != null){
+//                slow = slow.next;
+//                fast = fast.next.next;
+//                if(slow==fast){
+//                    // we have found a cycle
+//                    // now get the length -> stop the fast and continue the slow
+//                    int len = 0;
+//                    do{
+//                        slow = slow.next;
+//                        len++;
+//                    }while(slow!= fast);
+//                    // now you have the len. start 2 pointers
+//                    ListNode first = head;
+//                    ListNode second = head;
+//                    for(int i=1; i<=len; i++){
+//                        second = second.next;
+//                    }
+//
+//                    // now traverse equally until they reach the same point
+//                    while(first != second){
+//                        first  = first.next;
+//                        second = second.next;
+//                    }
+//                    return first; // or second
+//                }
+//            }
+//            return null;
+//
+//        }
+
+        // Q.6 Finding the middle of the linked list
+//        public ListNode middleNode(ListNode head) {
+//            ListNode slow = head;
+//            ListNode fast = head;
+//            while(fast != null  && fast.next != null){
+//                slow = slow.next;
+//                fast = fast.next.next;
+//            }
+//            return slow;
+//        }
+//
+
+        // Q7 Reversing a linked list using iterative method
+//        public ListNode reverseList(ListNode head) {
+//            ListNode p = null;
+//            ListNode c = head;
+//            ListNode n;
+//
+//            while(c != null){
+//                n = c.next;
+//                c.next = p;
+//                p = c;
+//                c = n;
+//            }
+//
+//            return p;
+//
+//        }
+
+        // Q8 Reversing a linked list using recursive approach
+//        public ListNode reverseList(ListNode head) {
+//
+//            // base case
+//            if(head == null || head.next == null){
+//                return head;
+//            }
+//            // implementation
+//            ListNode newHead = reverseList(head.next);
+//            head.next.next = head;
+//            head.next = null;
+//            return newHead;
+//        }
+
+
+        // Q9 Finding the palindrome in the linked list
+        // approach -> find the middle point, reverse ahead of the mid and compare with two pointer
+//        public boolean isPalindrome(ListNode head) {
+//            // approach -> find the middle point, find the reverse linked List and compare till mid
+//            if(head==null||head.next==null) return true;
+//
+//            ListNode slow = head;
+//            ListNode fast = head;
+//
+//            while(fast.next!=null&&fast.next.next!=null) {
+//                slow = slow.next;
+//                fast = fast.next.next;
+//            }
+//
+//            slow = reverseList(slow.next);
+//
+//            ListNode dummy = head;
+//
+//            while(slow!=null) {
+//                if(dummy.val != slow.val) return false;
+//                dummy = dummy.next;
+//                slow = slow.next;
+//            }
+//            return true;
+//        }
+
+
+        // Q10. Happy Number? if the square sums equals to one.
+//        public boolean isHappy(int n) {
+//            if(n==1) return true;
+//            int slow = n;
+//            int fast = n;
+//
+//            do{
+//                slow = squareSum(slow);
+//                fast = squareSum(squareSum(fast));
+//                if(slow==1 || fast == 1) return true;
+//            }while(slow!=fast);
+//            return false; // we reach here if cycle exists.
+//        }
+//
+//        public int squareSum(int n){
+//            int sum = 0;
+//            while(n!=0){
+//                int dig = n%10;
+//                sum += dig*dig;
+//                n = n/10;
+//            }
+//            return sum;
+//        }
 
 
     }
