2ad8a83b3390561c442cdd7c078c35ed113c9a64
aniruddhdadhich <aniruddhddh17@gmail.com>
2023-10-22 21:12:10 +0530

    Restarting LinkedList

--- Linked List/src/Main.java
+++ Linked List/src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-            DLL dll = new DLL();
 
-            dll.insertFirst(12);
-            dll.insertFirst(13);
-            dll.insertFirst(10);
-            dll.display();
-            dll.insertAt(25, 2);
-            dll.display();
-
-            int len = dll.getSize();
-            System.out.println(len);
     }
 }
 
